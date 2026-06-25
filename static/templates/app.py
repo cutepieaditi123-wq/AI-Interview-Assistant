9c8ed89fab51fa93c54239241f68073fb925d806
Aditi Sharma <cutepieaditi123@gmail.com>
2026-06-26 00:42:24 +0530

    Create app.py

---
+++ static/templates/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/interview")
+def interview():
+    return render_template("interview.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+def check_answer(question, answer):
+    answer = answer.lower()
+
+    if "python" in question.lower():
+        if "programming" in answer or "language" in answer:
+            return {
+                "score": 9,
+                "feedback": "Excellent Answer!"
+            }
+        else:
+            return {
+                "score": 4,
+                "feedback": "Try explaining that Python is a high-level programming language."
+            }
+
+    return {
+        "score": 7,
+        "feedback": "Good Attempt!"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
