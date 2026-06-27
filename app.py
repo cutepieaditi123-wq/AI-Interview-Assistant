d14f76179f00178d019d9993ff9276372687c109
Aditi Sharma <cutepieaditi123@gmail.com>
2026-06-27 19:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
 @app.route("/interview")
 def interview():
     return render_template("interview.html")
+
 
 def check_answer(question, answer):
     answer = answer.lower()
@@ -17,22 +17,18 @@ def check_answer(question, answer):
     if "python" in question.lower():
         if "programming" in answer or "language" in answer:
             return {
-                "score": 9,
-                "feedback": "Excellent Answer!"
+                "score": 10,
+                "feedback": "✅ Excellent Answer!"
             }
         else:
             return {
-                "score": 4,
-                "feedback": "Try explaining that Python is a high-level programming language."
+                "score": 5,
+                "feedback": "⚠️ Python is a high-level programming language."
             }
 
     return {
         "score": 7,
-        "feedback": "Good Attempt!"
+        "feedback": "👍 Good Attempt!"
     }
 
-import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
